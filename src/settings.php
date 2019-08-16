9c6932ffd6128e770df309242968339cbecf9e6a
y-fukumoto <y-fukumoto@ceres-inc.jp>
2019-08-16 19:08:08 +0900

    fix_setteing

--- src/settings.php
+++ src/settings.php
@@ -31,11 +31,11 @@ return [
 
             'connection' => [
                 'driver' => 'pdo_mysql',
-                'host' => 'localhost',
+                'host' => '127.0.0.1',
                 'port' => '3306',
                 'user' => 'team-c',
                 'password' => 'team-c!',
-                'dbname' => 'teamc',
+                'dbname' => 'team-c',
                 'charset' => 'utf8'
             ]
         ]

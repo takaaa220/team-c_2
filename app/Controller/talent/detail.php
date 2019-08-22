d4fe90a94ee8896583edb996fee0070f527c7028
Suyama@ZQ875456 <31335107+SuyamaZQ875456@users.noreply.github.com>
2019-08-22 14:06:11 +0900

    ページアクセス改善

--- app/Controller/talent/detail.php
+++ app/Controller/talent/detail.php
@@ -5,7 +5,7 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 
 // 有名人詳細
-$app->get('/talent/{talent_id}', function (Request $request, Response $response, $args) {
+$app->get('/talent/{talent_id}/', function (Request $request, Response $response, $args) {
     $data = [];
 
     return $this->view->render($response, 'talent/detail.twig', $data);

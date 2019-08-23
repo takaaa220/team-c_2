e8b673b369129db1860dbf9776fe7cfc9ff80d21
takaaa220 <taaaaaaaaaa220@gmail.com>
2019-08-23 13:55:44 +0900

    create apply function

--- app/Controller/talent/detail.php
+++ app/Controller/talent/detail.php
@@ -1,6 +1,6 @@
 <?php
 
-use Model\Dao\Item;
+use Model\Dao\OfferPost;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
@@ -13,4 +13,32 @@ $app->get('/talent/{talent_id}/', function (Request $request, Response $response
 
     return $this->view->render($response, 'talent/detail.twig', $data);
 
+});
+
+$app->post('/talent/{talent_id}/apply', function (Request $request, Response $response, $args) {
+    $talent_id = $args["talent_id"];
+    $param = $request->getParsedBody();
+
+    // ログインなかったらトップへ
+    $user = $this->session->get('user_info');
+    if ($user == "") {
+        return $response->withRedirect('/');
+    }
+
+    $offer_post = new OfferPost($this->db);
+
+    $param["usr_id"] = $user["id"];
+    $result = $offer_post->create($param);
+
+    if ($result == null) {
+        return $response->withRedirect("/talent/${talent_id}/");
+    }
+
+    return $response->withRedirect("/talent/${talent_id}/payment");
+});
+
+$app->get('/talent/{talent_id}/payment', function (Request $request, Response $response, $args) {
+    $data = [];
+
+    return $this->view->render($response, 'talent/payment.twig', $data);
 });

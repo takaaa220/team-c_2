1763de5b59e98120ebfd0deeeca995c208dfd477
Katsuma-F <k.fujita.indigo1003@gmail.com>
2019-08-23 14:05:40 +0900

    有名人詳細実装

--- app/Controller/talent/detail.php
+++ app/Controller/talent/detail.php
@@ -1,12 +1,23 @@
 <?php
 
-use Model\Dao\Item;
+use Model\Dao\Talent;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
 // 有名人詳細
-$app->get('/talent/{talent_id}/', function (Request $request, Response $response, $args) {
+$app->get('/talent/detail/{talent_id}', function (Request $request, Response $response, $args) {
+
     $data = [];
+    //URLパラメータのtalent_idを取得します。
+    $talent_id = $args["talent_id"];
+
+    $talent = new Talent($this->db);
+    $talent2 = new Talent($this->db);
+
+    //URLパラメータのtalent_id部分を引数として渡し、戻り値をresultに格納します
+    $data["result"] = $talent->getTalent($talent_id);
+    $data["category_result"] = $talent2->getTalentCategory($talent_id);
+// dd($data["category_result"]);
 
     return $this->view->render($response, 'talent/detail.twig', $data);
 
--- app/Model/Dao/Talent.php
+++ app/Model/Dao/Talent.php
@@ -63,4 +63,62 @@ class Talent extends Dao
 
     }
 
+
+    public function getTalentList()
+    {
+
+        //全件取得するクエリを作成
+        $sql = "select * from talent_master";
+
+        // SQLをプリペア
+        $statement = $this->db->prepare($sql);
+
+        //SQLを実行
+        $statement->execute();
+
+        //結果レコードを全件取得し、返送
+        return $statement->fetchAll();
+
+    }
+
+    public function getTalent($id)
+    {
+
+        //全件取得するクエリを作成
+          $sql = "select * from talent_master where id =:id";
+
+        // SQLをプリペア
+        $statement = $this->db->prepare($sql);
+
+        //idを指定します
+        $statement->bindParam(":id", $id, PDO::PARAM_INT);
+
+        //SQLを実行
+        $statement->execute();
+
+        //結果レコードを一件取得し、返送
+        return $statement->fetch();
+
+    }
+
+    public function getTalentCategory($id){
+
+      //全件取得するクエリを作成
+        $sql = "select * from talent_category where talent_id =:id";
+
+      // SQLをプリペア
+      $statement = $this->db->prepare($sql);
+
+      //idを指定します
+      $statement->bindParam(":id", $id, PDO::PARAM_INT);
+
+      //SQLを実行
+      $statement->execute();
+
+      //結果レコードを一件取得し、返送
+      return $statement->fetchAll();
+
+    }
+
+
 }
